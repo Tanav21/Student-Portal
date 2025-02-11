dac4a1df1c4f234d72c9ce1bf9026f3f053063e9
tanav <tanav@androwebstech.com>
2025-02-11 23:20:21 +0530

    changes

--- student_dashboard/src/Components/Header.jsx
+++ student_dashboard/src/Components/Header.jsx
@@ -1,27 +1,33 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Header = () => {
-  return <navbar>
-      <div class='w-full py-3 border-b'>
-
-        <div class='flex justify-between px-20 items-center font-semibold'>
+  return (
+    <navbar>
+      <div class="w-full py-3 border-b">
+        <div class="flex justify-between px-20 items-center font-semibold">
           <div>
             <h1 class="text-2xl">LOGO</h1>
           </div>
-          <div class='flex xl:gap-10 md:gap-8  gap-2'>
-            <a href="">Home</a>
+          <div class="flex xl:gap-10 md:gap-8  gap-2">
+            <Link to="/">Home</Link>
             <a href="">Work</a>
             <a href="">About</a>
             <a href="">Services</a>
             <a href="">Testimonial</a>
           </div>
           <div>
-            <button class='py-2 px-6 bg-black text-white rounded-3xl font-semibold'>Contact</button>
+            <Link
+              to="/login"
+              className="py-2 px-6 bg-black text-white rounded-3xl font-semibold"
+            >
+              Login
+            </Link>
           </div>
         </div>
-        
       </div>
     </navbar>
+  );
 };
 
 export default Header;
--- student_dashboard/src/Pages/HomePage.jsx
+++ student_dashboard/src/Pages/HomePage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PageLayout from "./PageLayout";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <PageLayout>
+        <div>HomePage</div>
+      </PageLayout>
+    </div>
+  );
 };
 
 export default HomePage;
