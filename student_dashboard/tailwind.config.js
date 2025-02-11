ed19f4ae24f20bb3947fe816e08c44e4eeac3c65
Sahil <sahilluthra495@gmail.com>
2025-02-11 22:00:34 +0530

    changes

--- student_dashboard/tailwind.config.js
+++ student_dashboard/tailwind.config.js
@@ -1,6 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
