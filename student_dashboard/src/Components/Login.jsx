4aa2930eae7f7b5479762d5d79d7f227b5adbf85
tanav <tanav@androwebstech.com>
2025-02-12 18:12:21 +0530

    fix

--- student_dashboard/src/Components/Login.jsx
+++ student_dashboard/src/Components/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
             <h1 className="text-3xl font-bold text-center  text-gray-800 mb-6">
               Login
             </h1>
-            <form onSubmit={handleSubmit} className="space-y-6">
+            <form className="space-y-6">
               <div>
                 <label
                   htmlFor="email"
