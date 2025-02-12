ac65f2adea09b35870e612af747b6d81cc549a25
tanav <tanav@androwebstech.com>
2025-02-12 13:32:07 +0530

    changes

--- student_dashboard/src/Components/SignUp.jsx
+++ student_dashboard/src/Components/SignUp.jsx
@@ -43,10 +43,10 @@ const SignUp = () => {
 
   return (
     <PageLayout>
-      <main className="flex justify-center items-center min-h-screen dark:bg-gray-900 bg-gray-100">
+      <main className="flex justify-center items-center min-h-screen  bg-gray-100">
         <section className="w-full max-w-md p-1 rounded-2xl bg-gradient-to-r from-blue-500 to-purple-500 shadow-lg m-4">
-          <div className="p-6 rounded-xl bg-white dark:bg-gray-900">
-            <h1 className="text-3xl font-bold text-center dark:text-gray-200 text-gray-800 mb-6">
+          <div className="p-6 rounded-xl bg-white ">
+            <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
               Create An Account
             </h1>
             <form onSubmit={handleSubmit} className="space-y-6">
@@ -106,14 +106,14 @@ const SignUp = () => {
               </div>
 
               <button class="w-full relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                <span class="w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                <span class="w-full relative px-5 py-2.5 transition-all ease-in duration-75 text-gray-900 bg-white rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent group-hover:text-white">
                   Sign Up
                 </span>
               </button>
             </form>
 
             <div className="text-center mt-6">
-              <p className="dark:text-gray-300">
+              <p className="dark:text-gray-700">
                 Already have an account?{" "}
                 <button
                   onClick={handleNavigateToLogin}
